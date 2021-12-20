3e4e770e26322843aed9eb657d9292ecd6a5d11b
Victor <joaumvictor.oficial@gmail.com>
2021-12-19 22:41:36 -0300

    dia 5.2 terminado

---
+++ fundamentos_do_desenvolvimento_web/bloco_5_JavaScript_DOM_e_seletores/dia_5.2/script1.js
@@ -0,0 +1,16 @@
+var p1 = document.getElementById("paragraph");
+var p2 = document.getElementById("second-paragraph");
+var t2 = document.getElementById("subtitle");
+var t1 = document.getElementById("page-title");
+
+p1.style.color = "brown";
+p1.innerText = "Piratas do caríbe é íncrivel e eu posso provar!";
+
+
+p2.style.color = "brown";
+p2.innerText = "Melhores filmes da história!";
+
+
+t1.innerText = "testando comandos rapidão kk";
+t2.innerText = "Hulk Esmaga";
+
--- fundamentos_do_desenvolvimento_web/bloco_5_JavaScript_DOM_e_seletores/dia_5.2/seiqla.js
+++
@@ -1,8 +0,0 @@
-let dentro = document.createElement('h1');
-dentro.innerText = 'Exercicio 5.2 - JavaScript e DOM';
-dentro.className = 'banido';
-document.body.appendChild(dentro);
-
-
-
-
