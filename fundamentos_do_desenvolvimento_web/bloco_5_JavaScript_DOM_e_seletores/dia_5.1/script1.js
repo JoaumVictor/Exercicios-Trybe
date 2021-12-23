73996f00ff400676ef5428f145949565179136c1
Victor <joaumvictor.oficial@gmail.com>
2021-12-22 23:10:05 -0300

    continuaçao

---
+++ fundamentos_do_desenvolvimento_web/bloco_5_JavaScript_DOM_e_seletores/dia_5.1/script1.js
@@ -0,0 +1,7 @@
+var t = document.body.querySelectorAll(".titles")
+
+t[0].style.color = "blue";
+t[0].innerText = "Não sei Rick, parece falso!"
+
+t[1].style.color = "blue";
+t[1].innerText = "Dificuldade do caramba";
