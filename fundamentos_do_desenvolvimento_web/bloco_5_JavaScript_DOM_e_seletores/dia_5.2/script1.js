afbd615dfdee82adc36f902be04092342229eef5
João Victor <joaumvictor.oficial@gmail.com>
2022-01-03 12:25:37 -0300

    dia 5.2 terminado

--- fundamentos_do_desenvolvimento_web/bloco_5_JavaScript_DOM_e_seletores/dia_5.2/script1.js
+++
@@ -1,7 +0,0 @@
-var t = document.body.querySelectorAll(".titles")
-
-t[0].style.color = "blue";
-t[0].innerText = "Não sei Rick, parece falso!"
-
-t[1].style.color = "blue";
-t[1].innerText = "Dificuldade do caramba";
